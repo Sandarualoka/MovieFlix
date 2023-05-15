ae0a0b9d320987b9472a5b8ef7e3782125dd928c
Sandaru  De Silva <sandarualoka99@gmail.com>
2023-05-15 11:59:59 +0530

    First commit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -16,7 +16,7 @@ const Row = ({ title, fetchURL }) => {
   }, [fetchURL]);
 
   console.log(movies);
-
+// return ;
   return (
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
